dbdc4a6faa559a5d8962c49183ce8237d58fe9d4
KI JONG BYUN <kjbyun0@gmail.com>
2024-06-26 15:43:18 -0500

    Add settings for PostgreSQL Database and WebService on Render

--- server/config.py
+++ server/config.py
@@ -9,11 +9,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
 
+import os
+
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
